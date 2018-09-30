a4ee4b399dec6b7ec3cc2f187b69137fde1f937c
brak1 <jbvance_imap@hotmail.com>
2018-09-30 02:25:35 -0500

    worked on making a noisy dataset and then smoothing it to test code operation

--- randompoints.py
+++ randompoints.py
@@ -5,44 +5,60 @@ from scipy.signal import savgol_filter
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+from math import sqrt
 
 n=1000
 D=40.0
 p=np.linspace(0,100,n)
-s=(D+p*p/(0.002*n*n))
+z=D+(n/2)*pow((p/n),2)
+s=D+(n/2)*pow((p/n),2)
 
 for d in range(0,n):
 	r=0.002*(random.randint(0,1000)-500)
-	s[d]=s[d]+(r*r)
+	s[d]=s[d]+r
 
-shat = savgol_filter(s, 81, 2) # window size 51, polynomial order 3import matplotlib.pyplot as plt
-
-fig=plt.figure()
-#plt.plot(p, s)
-plt.plot(s)
-plt.show()
-
-fig2=plt.figure()
-plt.plot(shat)
-plt.show()
-
-
-'''
-n=100
-x0=np.linspace(-np.pi, np.pi, n)
-y0=np.linspace(-np.pi, np.pi, n) 
-
-x,y=np.meshgrid(x0,y0)
-z=np.sin(x)*0.001*random.randint(0,1000)+np.cos(y)*0.001*random.randint(0,1000)
+s_smooth = savgol_filter(s, 401, 2) # window size 51, polynomial order 3import matplotlib.pyplot as plt
+s_smooth2 = savgol_filter(s,51,4)
+s_smooth3 = savgol_filter(s,101,2)
 
 fig=plt.figure()
-ax=fig.add_subplot(111,projection='3d')
-ax.plot_wireframe(x,y,z,rstride=5,cstride=5)
-#plt.show()
+fig.suptitle("Plotted Curve with Noise and Smoothing Samples")
+fig.subplots_adjust(left=0.1, wspace=0.4, hspace = 0.4)
 
+sub1=fig.add_subplot(221)
+sub1.plot(p,s)
+sub1.plot(p,z)
+sub1.set_title("'s' with noise and original 'z'")
 
-for angle in range(0,360):
-	ax.view_init(30,angle)
-	plt.draw()
-	plt.pause(.001)
-'''
+sub2=fig.add_subplot(222)
+sub2.plot(p,s_smooth)
+sub2.plot(p,z)
+sub2.set_title("s_smooth")
+
+sub3=fig.add_subplot(223)
+sub3.plot(p,s_smooth2)
+sub3.plot(p,z)
+sub3.set_title("s_smooth2")
+
+sub4=fig.add_subplot(224)
+sub4.plot(p,s_smooth3)
+sub4.plot(p,z)
+sub4.set_title("s_smooth3")
+
+total_len=0; total_len2=0; total_len3=0; total_len4=0; total_lenp=0; total_lenz=0;  
+for i in range(0,n-1):
+	total_len = total_len + sqrt(pow(s[i+1]-s[i],2) + pow(p[i+1]-p[i],2))
+	total_len2 = total_len2 + sqrt(pow(s_smooth[i+1]-s_smooth[i],2) + pow(p[i+1]-p[i],2))
+	total_len3 = total_len3 + sqrt(pow(s_smooth2[i+1]-s_smooth2[i],2) + pow(p[i+1]-p[i],2))
+	total_len4 = total_len4 + sqrt(pow(s_smooth3[i+1]-s_smooth3[i],2) + pow(p[i+1]-p[i],2))
+	total_lenp = total_lenp + (p[i+1]-p[i])
+	total_lenz = total_lenz + sqrt(pow(z[i+1]-z[i],2) + pow(p[i+1]-p[i],2))
+	
+print "length of 's' = ", total_len
+print "length of 's_smooth' = ", total_len2
+print "length of 's_smooth2' = ", total_len3
+print "length of 's_smooth3' = ", total_len4
+print "length of 'p' = ", total_lenp
+print "length of 'z' = ", total_lenz
+
+plt.show()
