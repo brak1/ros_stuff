fdb870e53672a1edcabac7451756673ac4d50ebb
brak1 <jbvance_imap@hotmail.com>
2018-10-12 23:50:38 -0500

    working on a ros

---
+++ ws_rosthings/src/rick_things/keyboard_node.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import serial, datetime, click, time, sys, select, termios, tty
+import rospy
+
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+if __name__=="__main__":
+    settings = termios.tcgetattr(sys.stdin)
+    
+    try:
+
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+#     while not rospy.is_shutdown():
+    hello_str = "hello world %s" % rospy.get_time()
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+  
+        while True:
+            key = getKey()
+            if key == "4":
+                print key + " pressed"
+                pub.publish(key)
+
+            elif key == "6":
+                print key + " pressed"
+                pub.publish(key)
+
+            elif key == "\x03":
+                break
+            
+            else:
+                print key + " pressed"
+                pub.publish(key)
+
+    except KeyboardInterrupt:
+        print(" keyboard interrupt received...")
+    finally:
+        print "Cleaned up program and exiting"
