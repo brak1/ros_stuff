2f61fa9ec215d72655a7ce44c709bb5d3e77625b
brak1 <jbvance_imap@hotmail.com>
2018-10-19 16:09:24 -0500

    edits to process a csv file for smoothing data

--- write_csv.py
+++ write_csv.py
@@ -14,11 +14,25 @@ x=np.linspace(0,100,n)
 y=(n/2)*pow(((x-50)/n),2)
 y_noise=(n/2)*pow(((x-50)/n),2)
 for d in range(0,n):
-    M=0.0002
+    M=0.0004
     r1=M*(random.randint(0,1000)-500)
     r2=0.01*random.randint(0,100)
     y_noise[d]=y_noise[d]+r1
-y_smooth = savgol_filter(y_noise,101,2)
+y_smooth = savgol_filter(y_noise,601,2)
+
+total_len_x=0; total_len_y_raw=0; total_len_y_smooth=0; 
+print len(x)
+print len(y_noise)
+print len(y_smooth)
+
+for i in range(0,len(x)-1):
+    total_len_x = total_len_x + sqrt(pow(float(x[i+1]) - float(x[i]),2))
+    total_len_y_raw = total_len_y_raw + sqrt(pow(float(y_noise[i+1])-float(y_noise[i]),2) + pow(float(x[i+1]) - float(x[i]),2))
+    total_len_y_smooth = total_len_y_smooth + sqrt(pow(float(y_smooth[i+1])-float(y_smooth[i]),2) + pow(float(x[i+1])-float(x[i]),2))
+
+print "total_len_x is:  ", str(total_len_x)
+print "total_len_y_raw is:  ", str(total_len_y_raw)
+print "total_len_y_smooth is:  ", str(total_len_y_smooth)
 
 fig=plt.figure()
 fig.suptitle("Plotting some crap I'm trying out")
